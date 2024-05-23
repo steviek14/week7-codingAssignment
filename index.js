354ee3966055b19c88c1c859970bb67e63c3031b
Elise (Stevie) Gordon <166882657+steviek14@users.noreply.github.com>
2024-05-23 05:25:09 +0000

    finishing touches

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ console.log('Part 1:');
 console.log("1A. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93")
 let agesArray = [3, 9, 23, 64, 2, 8, 28, 93]
    console.log(agesArray);
-   let firstValue = agesArray[0]; //declare a variable firstValue to find the first element in the agesArray
-   let lastIndex = agesArray.length-1; //declare a variable lastIndex to find the last element in agesArray
-   let lastValue = agesArray[lastIndex]; // decalre a variable lastValue set to the variable lastIndex which finds the last element in the array
+   let firstValue = agesArray[0]; // find the first element in the agesArray
+   let lastIndex = agesArray.length-1; //find the last element in agesArray
+   let lastValue = agesArray[lastIndex]; // finds the last element in the array
    let result = lastValue - firstValue; // subtract the last element in the array from the first
        console.log("1B. Subtract the value of the first element in the array from the value in the last element of the array:", result) ; //prints 90
    agesArray.push(56) //adding a new age
        console.log("1C. Add a new age then calculate result again:", result); //prints 90
-let sum = 0 //declare a sum set to 0 - starting our sum count
+let sum = 0 //starting our sum count
    for (let i = 0; i <agesArray.length; i++) {
    sum += agesArray[i]
-   } // for loop that adds 1 count to the sum and continues to iterate as long as the sum is smaller than the arrays length
+   } // for loop to find accumulated sum 
 let average = sum / agesArray.length // taking the accumlated sum of the array and dividing by how many elements are in the array to get the average
 console.log("1D. Calculate the average age of this array:", average); //calculates average...printing 31.77777777777778
 
@@ -25,19 +25,21 @@ console.log("1D. Calculate the average age of this array:", average); //calculat
 console.log('Part 2:');
 let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck','Bob'] //declare the array names
 console.log("2A. Declare an array called names that contaisn the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.", names)
+
 let totalLetters = 0 //decalre a variable that keeps count of the total letters in the array
    for (let i = 0; i < names.length; i++){
        totalLetters += names[i].length
-   } // this will iterate over the array as long as the total letters is less than the names array length and it will add one count through each iteration
+   } // for loop to accumulate the total letters of each element in the array
    let letterAverage = totalLetters / names.length; //taking the total letters and the length of the array we can find the average of the array
+   
    console.log("2B. Calculate the average number of letters per name:", letterAverage);
 
 
    let concatNames = ''
        for (let i= 0; i < names.length; i++) {
-           concatNames += names[i] + ' '
+           concatNames += names[i] + ' ' //for loop that concats the name array together 
        }
-       concatNames = concatNames.trim();
+       concatNames = concatNames.trim(); //removes whitespace 
        console.log("2C. Concatenate all the names together, seperated by spaces:", concatNames);
 
 
@@ -54,18 +56,18 @@ console.log('someArray.length-1');
 
 //Part 4:
 console.log('Part 4:')
-console.log("How do you access the first element if any array?")
-console.log('anotherArray[0]')
+console.log("How do you access the first element if any array?") //question
+console.log('anotherArray[0]') //answer 
 
 
 
 
 //Part 5:
 console.log("Part 5:")
-var namesLengths = [ ] //declare an empty array to later push the results of the loop
-for (let i = 0; i < names.length; i++){ //this loop iterates until i is less than the length of the sum of the array names
+var namesLengths = [ ] // empty array to later push the results of the loop
+for (let i = 0; i < names.length; i++){ //for loop to find the length of each element in the names array 
    namesLengths.push(names[i].length)
-} //we are then pushing the result of the for loop to the empty array
+} //pushing the result of the for loop to the empty array
 console.log(" Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the lengths of each anme to the nameLengths array.", namesLengths)
 
 
@@ -73,10 +75,10 @@ console.log(" Create a new array called nameLengths. Write a loop to iterate ove
 
 //Part 6:
 console.log("Part 6: ")
-let sum2 = 0 //declare a sum set to 0 - starting our sum count
+let sum2 = 0 //starting our sum count
    for (let i = 0; i <namesLengths.length; i++) {
    sum2 += namesLengths[i]
-   } //I did this above initally for part 2 but assigned it to namesLength array
+   } //similar to above for loop for Part 1 -- find the length of each element in the namesLengths array 
    console.log('Calculate the sum of all the elements in the array: ', sum2);
 
 
@@ -88,7 +90,7 @@ let sum2 = 0 //declare a sum set to 0 - starting our sum count
 console.log('Part 7: ')
 let function1 = (word, n) => {
    let result = ' ';
-   for (let i =0; i < n; i++) //tells the loop to repeat the iteration of the word n number of times
+   for (let i =0; i < n; i++) 
        result += word // the result is word printing out itself 3 times as set for the loop conditions
    return result //returns the string concatenated with itself the number of times that n is
 }
@@ -105,7 +107,7 @@ let function2 = (firstName, lastName) => {
 console.log('Return the full name seperated by a space: ',function2("Stevie", 'Gordon')) //calling the functiona and passing in the arguments
 
 
-//Part 9
+//Part 9 Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100. 
 console.log('Part 9: ')
 function isSumGreaterThan100(numbers) {
    // Calculate the sum of all numbers in the array using reduce method
@@ -114,14 +116,14 @@ function isSumGreaterThan100(numbers) {
    // Check if the sum is greater than 100
    return sum > 100;
 }
-let numbers1 = [30, 40, 50]; // Sum = 120
+let numbers1 = [30, 40, 50]; // Sum = 120 prints true 
 console.log(isSumGreaterThan100(numbers1));
 
 
 //Part 10
 console.log('Part 10: ')
 function calculateAverage(numbers) {
-   // Calculate the sum of all numbers in the array
+   // Calculate the sum of all numbers in the array numbers2
    let sum = numbers.reduce((acc, curr) => acc + curr, 0);
   
    // Calculate the average by dividing the sum by the number of elements in the array
@@ -149,7 +151,7 @@ function compareAverages(array1, array2) {
 }
 
 
-// Function to calculate average (as provided in the previous response)
+// Function to calculate average 
 function calculateAverage(numbers) {
    if (numbers.length === 0) {
        return 0;
@@ -172,7 +174,7 @@ function willBuyDrink (isHotOutside, moneyInPocket) {
    if (isHotOutside === true && moneyInPocket > 10.50) {
        return true
    }
-}
+} // function will return true if isHotOutside is true and moneyInPocket is more than 10.50
 console.log("Is it hot outside? Do I have more than $10.50?", willBuyDrink(true, 75))
 
 
